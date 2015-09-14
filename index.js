c764775106555dd67e0f352690ca4a4a52aabb0d
Peter Petrov <petrov.p.peter@gmail.com>
2015-09-14 10:50:14 -0400

    Setup rendering

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var ejs = require("ejs");
 
-app.get('/', function(req, res){
-	
-});
+var bodyParser = require('body-parser');
+var urlencodedBodyParser = bodyParser.urlencoded({extended: false});
+var methodOverride = require('method-override');
 
-http.listen(3000, function(){
+var app = express();
+
+app.use(urlencodedBodyParser);
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+
+app.set('view engine', 'ejs');
+
+app.listen(3000, function(){
 	console.log("listening");
 });
+
+
+app.get('/', function(req, res){
+    res.render("index.html.ejs");
+});
+
+
