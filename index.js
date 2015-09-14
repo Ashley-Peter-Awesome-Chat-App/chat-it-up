d4110beecacadac0ff4baf882551b5c98d01cf8d
Peter Petrov <petrov.p.peter@gmail.com>
2015-09-14 11:49:52 -0400

    Basic chat

--- index.js
+++ index.js
@@ -36,8 +36,12 @@ io.on("connection", function(socket) {
 
     socket.on('chat message', function(msg) {
         console.log(msg);
+        socket.broadcast.emit('chat message', msg);
     });
+    
 });
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,10 +1,18 @@
 console.log("Something wicked this way comes");
 $(function() {
+    
     var socket = io();
 
     $("#send").on("click", function(event){
         console.log("Hey");
-        socket.emit('chat message', $('#m').val());
+        var msg = $('#m').val();
+        socket.emit('chat message', msg);
         $('#m').val('');
+        
+        $("#messages").append($("<li>").text(msg));
+    });
+
+    socket.on('chat message', function(msg) {
+        $("#messages").append($("<li>").text(msg));
     });
 });
